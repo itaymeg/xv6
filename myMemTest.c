a746251dcc0f4d622d43d32107e28eaa56dd2c97
Ofir Farjun <farkash7@gmail.com>
2016-06-04 21:56:20 +0300

    finished!!!!!!!!!!!

--- myMemTest.c
+++ myMemTest.c
@@ -2,105 +2,56 @@
 #include "stat.h"
 #include "user.h"
 
+#define LOOP 20
+
+
 int
 main(int argc, char *argv[])
 {
-    int n=20;
-    void * pages[n];
+	void * pages[LOOP];
 
-    int pid1;
-    int pid2;
+	int firstFork;
+	int secondFork;
+	int i, j;
+	for (i = 0; i < LOOP; i++) {
+		pages[i]=sbrk(4096);
+	}
+	for (i = 0; i < LOOP; i++) {
+		*(int *)pages[i] = i;
+	}
+	firstFork = fork();
+	if (firstFork != 0){
+		for (i = 0; i < 5; i++) {
+			*(int *)pages[i] = i;
+		}
+		for (i = 0; i < 5; i++) {
+			*(int *)pages[i] = i;
+		}
+		for (i = 0; i < 5; i++) {
+			*(int *)pages[i] = i;
+		}
+		for (i = 0; i < 5; i++) {
+			*(int *)pages[i] = i;
+		}
+	}
+	for (i = 5; i < 15; i++) {
+		*(int *)pages[i] = i;
+	}
+	secondFork = fork();
+	for (j = 0; j < 5; j++){
+		for (i = 0; i < 5; i++) {
+			//printf(1,"accessing memory: pages[%d] = %d\n", i,*((int *)pages[i]));
+			if ((int *)pages[i]);
+		}
+	}
 
-    printf(1,"------- BEFORE FIRST FORK -------\n");
-    pid1=fork();
-    printf(1,"------- AFTER FIRST FORK -------\n");
+	if(firstFork > 0)
+		wait();
 
-    int i;
-    for (i=0; i<n; i++) {
-        pages[i]=sbrk(4096);
-        printf(1,"allocated page %d at address 0x%x for process %d\n",i,(int)pages[i],getpid());
-    }
+	if (secondFork > 0)
+		wait();
 
-       printf(1,"process %d finished allocating pages\n",getpid());
-
-       printf(1,"process %d access it's memory\n",getpid());
-    for (i=0; i<n; i++) {
-        *(int *)pages[i] = i;
-    }
-    printf(1,"process %d finished accessing it's memory\n",getpid());
-
-    printf(1,"------- BEFORE SECOND FORK -------\n");
-    pid2=fork();
-    printf(1,"------- AFTER SECOND FORK -------\n");
-
-    for (i=0; i<n; i++) {
-       printf(1," process %d is accessing it's memory: pages[%d] = %d\n",getpid(), i,*((int *)pages[i]) );
-    }
-
-    if(pid1>0)
-        wait();
-
-    if (pid2>0)
-        wait();
-
-   printf(1,"Finished Successfuly!!!\n");
-    exit();
+	printf(1,"End of Test\n");
+	exit();
 }
 
-
-
-//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-//
-//
-//#include "param.h"
-//#include "types.h"
-//#include "stat.h"
-//#include "user.h"
-//#include "fs.h"
-//#include "fcntl.h"
-//#include "syscall.h"
-//#include "traps.h"
-//#include "memlayout.h"
-//
-//#define PGSIZE 4096
-//#define LOOP 27
-//
-//char* m1[LOOP];
-//
-//volatile int
-//main(int argc, char *argv[])
-//{
-//
-//	int i,j;
-//	int k = 0;
-//
-//
-////	int pid;
-//	//creating 'COUNT' pages
-//	for (i = 0; i < LOOP ; ++i)
-//	{
-////		if (i% 2 == 0){
-////			if ((pid = fork()) == 0) break;
-////		}
-//		m1[i] = sbrk(PGSIZE);
-//		printf(1, "allocated page #%x at address: %x\n", i, m1[i]);
-//	}
-//
-//
-//	//using all pages
-//	for ( i = 0; i < LOOP; ++i)
-//	{
-//		for ( j = 0; j < PGSIZE; ++j)
-//		{
-//			m1[i][j] = 0;
-//			test:
-//			if (k == 0) {printf(1,"!!!!!!!!!!%x\n", &&test); k++;}
-//		}
-//	}
-//
-//
-//	printf(1,"Finished Successfuly!!!\n");
-//
-//	exit();
-//	return 0;
-//}
